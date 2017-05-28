2ba240383eb21908c9f3fae380e1c6dbb1d41735
ezec09@hotmail.com <ezec09@hotmail.com>
2017-05-28 16:25:21 -0300

    manejo unos errores, no se permiten indicadores repetidos y otros

--- src/main/java/dds/tp/model/GuardadorIndicadores.java
+++ src/main/java/dds/tp/model/GuardadorIndicadores.java
@@ -3,6 +3,8 @@ package dds.tp.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.management.RuntimeErrorException;
+
 public class GuardadorIndicadores {
 
 	private List<Indicador> indicadores;
@@ -16,7 +18,11 @@ public class GuardadorIndicadores {
 		return indicadores;
 	}
 	
-	public void addIndicador(Indicador indc) {
+	public void addIndicador(Indicador indc) throws RuntimeErrorException {
+		if(this.indicadores.stream().anyMatch(elem -> elem.getNombre().equalsIgnoreCase(indc.getNombre())))
+		{
+			throw new RuntimeErrorException(new Error("Ya existe un indicador con este nombre"));
+		}
 		this.indicadores.add(indc);
 	}
 }
