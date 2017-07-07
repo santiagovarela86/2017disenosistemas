4855eb8924f334c798bdb567b67c4d889ab953ec
ezec09@hotmail.com <ezec09@hotmail.com>
2017-07-07 12:51:28 -0300

    Agrego try catch en filtro por s tira error el evaluar

--- src/main/java/dds/tp/model/metodologia/Filtro.java
+++ src/main/java/dds/tp/model/metodologia/Filtro.java
@@ -3,6 +3,7 @@ package dds.tp.model.metodologia;
 import java.util.ArrayList;
 import java.util.List;
 
+import dds.tp.excepciones.ElementoNotFound;
 import dds.tp.model.CondicionTaxativa;
 import dds.tp.model.Empresa;
 import dds.tp.model.repositorios.RepositorioIndicadores;
@@ -12,8 +13,13 @@ public class Filtro {
 	public ArrayList<ResultadoAnalisis> getResultadosNegativos(List<Empresa> empresas, CondicionTaxativa condicion, RepositorioIndicadores repoIndicadores){
 		ArrayList<ResultadoAnalisis> empresasQueNoCumplen = new ArrayList<>();
 		for (Empresa empresa : empresas) {
-			if(!condicion.evaluar(empresa, repoIndicadores)){
-				empresasQueNoCumplen.add(new ResultadoAnalisis(0, empresa, "No cumple la condicion " + condicion.getNombre()));
+			try {
+				if(!condicion.evaluar(empresa, repoIndicadores)){
+					empresasQueNoCumplen.add(new ResultadoAnalisis(0, empresa, "No cumple la condicion " + condicion.getNombre()));
+				}
+			}
+			catch(ElementoNotFound e){
+				empresasQueNoCumplen.add(new ResultadoAnalisis(0, empresa, "No hay suficiente informacion para aplicar condiciones"));
 			}
 		}
 		return empresasQueNoCumplen;
@@ -22,8 +28,13 @@ public class Filtro {
 	public ArrayList<Empresa> getEmpresasQueCumplen(List<Empresa> empresas, CondicionTaxativa condicion, RepositorioIndicadores repoIndicadores){
 		ArrayList<Empresa> empresasQueCumplen = new ArrayList<>();
 		for (Empresa empresa : empresas) {
-			if(condicion.evaluar(empresa, repoIndicadores)){
-				empresasQueCumplen.add(empresa);
+			try {
+				if(condicion.evaluar(empresa, repoIndicadores)){
+					empresasQueCumplen.add(empresa);
+				}
+			}
+			catch(ElementoNotFound e) {
+				//Pass
 			}
 		}
 		return empresasQueCumplen;
