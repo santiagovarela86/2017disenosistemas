cb7c9a230d6ef749026a69d6831d923d12b3ec69
ezec09 <ezec09@hotmail.com>
2017-11-15 16:47:30 -0300

    Andando en heroku

--- src/main/java/dds/tp/Spark/Server.java
+++ src/main/java/dds/tp/Spark/Server.java
@@ -6,11 +6,19 @@ public class Server {
 	
 	public static void main(String[] args) {
 		
-		port(80);
+		port(getHerokuAssignedPort());
 		
 		staticFileLocation("/public");
 		
 		Router.start();
 		
 	}
+	
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
 }
---
+++ src/main/java/dds/tp/main/ProbandoRedis.java
@@ -0,0 +1,9 @@
+package dds.tp.main;
+
+public class ProbandoRedis {
+
+	public static void main(String[] args) {
+		
+	}
+	
+}
