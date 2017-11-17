f377e7c9e6b018ef1c4ecf6def4341e297d4184f
ezec09 <ezec09@hotmail.com>
2017-11-17 11:22:58 -0300

    dejo pom.xml como estaba

--- src/main/java/dds/tp/main/InicializarDB.java
+++ src/main/java/dds/tp/main/InicializarDB.java
@@ -27,8 +27,7 @@ public class InicializarDB {
 		guardarMetodologias(usuarioDefault);
 		guardarMetodologias(santiago);
 		MemoriaCache memCache = new MemoriaCache();
-		//memCache.inicializarCache(repoEmpresas.getEmpresas(), repositorioIndicadores, usuarioDefault);
-		memCache.nuevasEmpresas(repoEmpresas.getEmpresas());
+		memCache.inicializarCache(repoEmpresas.getEmpresas(), repositorioIndicadores, usuarioDefault);
 		System.exit(0);
 	}
 	
